8711198c81badaff4190d1f50e5dcb3097425725
smallqiang-PC <strongeren@163.com>
2018-01-29 22:27:38 +0800

    update

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.IntegerField(null=True)
+    author = models.ManyToManyField("Author")
+    publish = models.ForeignKey("Publish",on_delete=models.CASCADE)
+
+    class Meta: #设置表方法的源类
+        unique_together = (('name','price'),) #联合唯一
+
+class Author(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Publish(models.Model):
+    name = models.CharField(max_length=64)
